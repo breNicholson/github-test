90005830ab7e7fdcbd6895d4b26792a21c52db96
LCAir12 <lcair12@172-17-6-3.DYNAPOOL.NYU.EDU>
2015-07-28 10:50:09 -0400

    init commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Github Test!</title>
+	</head>
+	<body>
+		<header>
+			<h1></h1>
+			<h2></h2>
+		</header>
+		<section>
+			<p></p>
+		</section>
+		<footer>
+			<p></p>
+		<footer>
+	</body>
+</html>
